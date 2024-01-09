8af89ccc5e23904313cc29531b7feb749d772c6a
gtom06 <56637690+gtom06@users.noreply.github.com>
2024-01-09 11:30:10 +0100

    Update cryptogpt.py

--- src/cryptogpt.py
+++ src/cryptogpt.py
@@ -9,7 +9,7 @@ LOGGING_INTERVAL_DAYS = 7
 
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
-print('hello te')
+print('hello test')
 
 class Portfolio:
     REQ_PRICE = "https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=eur&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false&precision=full"
