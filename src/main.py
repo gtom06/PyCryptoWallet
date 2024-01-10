37aacdffb56adeb2d21b100c04ccb84bbb382ba2
gtom06 <giuseppe.tomassi@gmail.com>
2024-01-10 12:32:26 +0100

    mod main

--- src/main.py
+++ src/main.py
@@ -1,18 +1,37 @@
 import threading
 import time
+import sys
 
 from portfolio import Portfolio
 
 def main_loop(portfolio):
-    while True:
-        portfolio.calculate_and_save_portfolio_values()
-        time.sleep(120)
+    try:
+        while True:
+            portfolio.calculate_and_save_portfolio_values()
+            time.sleep(120)
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received. Exiting...")
+        sys.exit()
 
 def perform_operations(portfolio):
     while True:
-        # Esegui operazioni come add_crypto qui nel thread principale
-        portfolio.add_crypto("btc", 2, 200)  # Esempio di add_crypto
-        time.sleep(10)  # Esempio: attendi 10 secondi prima di eseguire la prossima operazione
+        try:
+            # Esegui operazioni come add_crypto qui nel thread principale
+            crypto_symbol = input("Enter the cryptocurrency symbol: ")
+            quantity = float(input("Enter the quantity: "))
+            price = float(input("Enter the price: "))
+
+            # Esempio di add_crypto con i parametri inseriti dall'utente
+            portfolio.add_crypto(crypto_symbol, quantity, price)
+            time.sleep(10)  # Esempio: attendi 10 secondi prima di eseguire la prossima operazione
+
+        except ValueError as e:
+            print(f"Error: {e}. Please enter valid numeric values for quantity and price.")
+            # You can choose to handle this error in a way that makes sense for your application.
+
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            # Handle other unexpected errors here.
 
 if __name__ == "__main__":
     portfolio = Portfolio()
